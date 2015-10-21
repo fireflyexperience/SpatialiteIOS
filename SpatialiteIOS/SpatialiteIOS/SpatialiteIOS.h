fcd520ee485235063ddc392191551f6a65ca6797
Andy Bennett <andybennett@Andys-MacBook-Pro.local>
2015-10-21 02:00:06 +0100

    initial setup

---
+++ SpatialiteIOS/SpatialiteIOS/SpatialiteIOS.h
@@ -0,0 +1,21 @@
+//
+//  SpatialiteIOS.h
+//  SpatialiteIOS
+//
+//  Created by Andy Bennett on 21/10/2015.
+//  Copyright © 2015 Firefly Experience Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpatialiteIOS.
+FOUNDATION_EXPORT double SpatialiteIOSVersionNumber;
+
+//! Project version string for SpatialiteIOS.
+FOUNDATION_EXPORT const unsigned char SpatialiteIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpatialiteIOS/PublicHeader.h>
+
+#include <sqlite3.h>
+#include <spatialite/gaiageo.h>
+#include <spatialite.h>
